1e4ee852ffd26e1dcc72e7b5847410bd094f5e05
Adrian <92375484+AdrianKCL@users.noreply.github.com>
2022-05-07 13:33:44 +0200

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -56,7 +56,7 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
                           min={0}
                           max={99}
                           className="cart__input"
-                          value=book.quantity}
+                          value={book.quantity}
                           onChange={(event) =>
                             changeQuantity(book, event.target.value)
                           }
